e55dd8fb259acf2ad0620b8d00da80e3c7892a4d
Lucas Ramage <lucas.ramage@infinite-omicron.com>
2023-06-03 17:44:30 -0500

    Create uci.go

---
+++ cmd/uci.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+  "fmt"
+
+  "github.com/spf13/cobra"
+)
+
+func init() {
+  getCmd.AddCommand(uciCmd)
+}
+
+var uciCmd = &cobra.Command{
+  Use:   "uci",
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("uci")
+  },
+}
